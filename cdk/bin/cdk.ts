97250359b0a69f2f2fe9e8813904fb5f629a383e
Raphaël Barbazza <5199489-rbarbazz@users.noreply.gitlab.com>
2022-03-30 23:06:56 +0000

    feat: backend signup endpoint

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,21 +1,28 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/cdk-stack';
+import { App } from 'aws-cdk-lib';
 
-const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+import {
+  ResourceConfig,
+  UserManagementStack
+} from '../lib/cdk-user-management-stack';
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const app = new App();
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+// Get the value of the current stage "dev" or "prod"
+const stage = app.node.tryGetContext('stage');
+// Get the config for the current stage
+const { resourceConfig }: { resourceConfig: ResourceConfig } =
+  app.node.tryGetContext(stage);
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+new UserManagementStack(
+  new App(),
+  `StreamHealthDashboardUserManagementStack-${stage}`,
+  {
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: process.env.CDK_DEFAULT_REGION
+    }
+  },
+  resourceConfig
+);
