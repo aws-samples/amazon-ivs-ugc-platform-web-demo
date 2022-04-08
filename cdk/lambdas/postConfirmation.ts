66a2a8ae52be0c7c5bda92efd6ab6e18050721f1
Raphaël Barbazza <5199489-rbarbazz@users.noreply.gitlab.com>
2022-04-08 16:06:36 +0000

    feat: create resources event trigger

---
+++ cdk/lambdas/postConfirmation.ts
@@ -0,0 +1,73 @@
+import {
+  ChannelType,
+  CreateChannelCommand,
+  IvsClient
+} from '@aws-sdk/client-ivs';
+import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
+import { PostAuthenticationTriggerHandler } from 'aws-lambda';
+
+import {
+  CHANNEL_CREATION_ERROR,
+  INVALID_INPUT_ERROR,
+  USER_CREATION_ERROR
+} from './constants';
+
+const ivsClient = new IvsClient({});
+const dynamoDbClient = new DynamoDBClient({});
+
+export const handler: PostAuthenticationTriggerHandler = async (
+  event,
+  _context,
+  callback
+) => {
+  const { request, userName } = event;
+  const { email, sub } = request.userAttributes;
+
+  if (!email || !userName) {
+    console.error(`Invalid input:\nemail: ${email}\nuserName: ${userName}\n`);
+
+    return callback(new Error(INVALID_INPUT_ERROR), null);
+  }
+
+  // Create IVS channel
+  const cleanedUserName = userName.replace(/[^a-zA-Z0-9-_]/g, '');
+  const channelName = `${cleanedUserName}s-channel`;
+  const createChannelCommand = new CreateChannelCommand({
+    name: channelName,
+    type: process.env.IVS_CHANNEL_TYPE as ChannelType
+  });
+  let channelArn;
+
+  try {
+    const { channel } = await ivsClient.send(createChannelCommand);
+    channelArn = channel?.arn;
+
+    if (!channelArn) {
+      throw new Error(`Empty ARN for channel: ${channelName}`);
+    }
+  } catch (error) {
+    console.error(error);
+
+    return callback(new Error(CHANNEL_CREATION_ERROR), null);
+  }
+
+  // Create entry in the user table
+  const putItemCommand = new PutItemCommand({
+    Item: {
+      channelArn: { S: channelArn },
+      email: { S: email },
+      id: { S: sub }
+    },
+    TableName: process.env.USER_TABLE_NAME
+  });
+
+  try {
+    await dynamoDbClient.send(putItemCommand);
+  } catch (error) {
+    console.error(error);
+
+    return callback(new Error(USER_CREATION_ERROR), null);
+  }
+
+  return callback(null, event);
+};
--- cdk/sanitize-output.js
+++ cdk/sanitize-output.js
@@ -4,9 +4,9 @@ const inputPath = 'temp_out.json';
 const json = readFileSync(inputPath);
 const output = JSON.parse(json);
 
-const { userManagementApiGatewayEndpoint } =
+const { userPoolClientId, userPoolId } =
   output[`StreamHealthDashboardUserManagementStack-${process.argv[2]}`];
-const publicCdkOutput = { userManagementApiGatewayEndpoint };
+const publicCdkOutput = { userPoolClientId, userPoolId };
 
 const outputPath = '../web-ui/src/cdk_output.json';
 writeFileSync(outputPath, JSON.stringify(publicCdkOutput));
