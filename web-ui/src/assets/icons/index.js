3eac54913f6a9ded0a417c269679037752a2cecb
Andrei Dobre <andrei@fournine.digital>
2022-05-03 03:36:29 +0000

    feat: floating menu

--- web-ui/src/assets/icons/index.js
+++ web-ui/src/assets/icons/index.js
@@ -2,6 +2,8 @@ export { ReactComponent as Check } from './check.svg';
 export { ReactComponent as ChevronLeft } from './chevron-left.svg';
 export { ReactComponent as ChevronRight } from './chevron-right.svg';
 export { ReactComponent as Error } from './error.svg';
+export { ReactComponent as Home } from './home.svg';
+export { ReactComponent as Logout } from './logout.svg';
 export { ReactComponent as Settings } from './settings.svg';
 export { ReactComponent as Visibility } from './visibility.svg';
 export { ReactComponent as VisibilityOff } from './visibility-off.svg';
--- web-ui/src/pages/Dashboard/Header/Header.jsx
+++ web-ui/src/pages/Dashboard/Header/Header.jsx
@@ -2,20 +2,17 @@ import { useLocation, useNavigate } from 'react-router-dom';
 
 import { dashboard as $content } from '../../../content';
 import { Settings } from '../../../assets/icons';
-import { userManagement } from '../../../api';
+import { useMobileBreakpoint } from '../../../contexts/MobileBreakpoint';
 import { useUser } from '../../../contexts/User';
 import Button from '../../../components/Button';
 import SessionNavigator from './SessionNavigator';
 import './Header.css';
 
 const Header = () => {
-  const { clearUserData } = useUser();
+  const { logOut } = useUser();
   const { pathname } = useLocation();
+  const { isMobileView } = useMobileBreakpoint();
   const navigate = useNavigate();
-
-  const handleLogOut = () => {
-    userManagement.signOut() && clearUserData();
-  };
 
   const handleSettings = () => {
     navigate(pathname === '/settings' ? -1 : '/settings');
@@ -24,18 +21,20 @@ const Header = () => {
   return (
     <header className="header">
       <SessionNavigator />
-      <div className="header-buttons">
-        <Button
-          className="settings-button"
-          onClick={handleSettings}
-          variant="secondary"
-        >
-          <Settings />
-        </Button>
-        <Button onClick={handleLogOut} variant="secondary">
-          {$content.header.log_out}
-        </Button>
-      </div>
+      {!isMobileView && (
+        <div className="header-buttons">
+          <Button
+            className="settings-button"
+            onClick={handleSettings}
+            variant="secondary"
+          >
+            <Settings className="icon settings" />
+          </Button>
+          <Button onClick={logOut} variant="secondary">
+            {$content.header.log_out}
+          </Button>
+        </div>
+      )}
     </header>
   );
 };
