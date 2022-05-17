2c0dcf718d8db446d9e795e76695917b81a9d43e
Andrei Dobre <andrei@fournine.digital>
2022-05-17 23:55:48 +0000

    feat: stream metrics structure

--- web-ui/src/pages/Dashboard/subpages/StreamSession/StreamSession.jsx
+++ web-ui/src/pages/Dashboard/subpages/StreamSession/StreamSession.jsx
@@ -1,8 +1,10 @@
 import EncoderConfiguration from './EncoderConfiguration';
+import Metrics from './Metrics';
 import './StreamSession.css';
 
 const StreamSession = () => (
   <article className="stream-session">
+    <Metrics />
     <EncoderConfiguration />
   </article>
 );
