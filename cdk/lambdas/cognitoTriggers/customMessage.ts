021cfd27bbc281ec666129728918a31ff898b2e5
rbarbazz <raphael@fournine.digital>
2022-05-18 07:48:13 -0700

    fix: confirmation email resent code as link

--- cdk/lambdas/cognitoTriggers/customMessage.ts
+++ cdk/lambdas/cognitoTriggers/customMessage.ts
@@ -7,7 +7,10 @@ export const handler: CustomMessageTriggerHandler = (
 ) => {
   if (event.triggerSource === 'CustomMessage_ForgotPassword') {
     event.response.emailMessage = `Use the link below to set up a new password for your Stream Health Dashboard account. This password reset link is only valid for the next 1 hour. If you did not make this request, you can safely disregard this email.\n<a href=${process.env.USER_MANAGEMENT_CLIENT_BASE_URL}/reset?code=${event.request.codeParameter}&username=${event.userName}>Reset Password</a>`;
-  } else if (event.triggerSource === 'CustomMessage_SignUp') {
+  } else if (
+    event.triggerSource === 'CustomMessage_SignUp' ||
+    event.triggerSource === 'CustomMessage_ResendCode'
+  ) {
     event.response.emailMessage = `Use the link below to confirm your email for your Stream Health Dashboard account.<a href=${process.env.USER_MANAGEMENT_CLIENT_BASE_URL}/login?code=${event.request.codeParameter}&username=${event.userName}>Confirm Email</a>`;
   }
 
