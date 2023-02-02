2bf29fd99819083c7cdae7b80c272bfb0bcc594f
Sina Keshmiri <sina.keshmiri@telus.com>
2023-02-02 13:17:43 -0800

    fix: update changeUserPreference endpoint
    Signed-off-by: Sina Keshmiri <sina.keshmiri@telus.com>
    Signed-off-by: Sina Keshmiri <sina@fournine.digital>

--- cdk/api/channel/authRouter/changeUserPreferences.ts
+++ cdk/api/channel/authRouter/changeUserPreferences.ts
@@ -13,131 +13,111 @@ import {
 import { UserContext } from '../authorizer';
 
 interface Preference {
-  name: string;
-  [key: string]: any;
-}
-
-interface AvatarPreference extends Preference {
+  name?: string;
   previewUrl?: string;
   uploadDateTime?: string;
 }
-interface ColorPreference extends Preference {}
 
 interface ChangeUserPreferencesRequestBody {
-  avatar?: AvatarPreference;
-  color?: ColorPreference;
+  [key: string]: Preference;
 }
-
-const CUSTOM_AVATAR_NAME = 'custom';
 
 const handler = async (
   request: FastifyRequest<{ Body: ChangeUserPreferencesRequestBody }>,
   reply: FastifyReply
 ) => {
   const { sub, username } = request.requestContext.get('user') as UserContext;
+  const { body: preferences } = request;
 
-  const preferencesToUpdate = Object.entries(request.body).reduce<
-    Record<string, Preference>
-  >(
-    (preferences, [key, value]) =>
-      !!value?.name ? { ...preferences, [key]: value } : preferences,
-    {}
-  );
-
-  if (!Object.keys(preferencesToUpdate).length) {
+  if (!Object.keys(preferences).length) {
     throw new Error(`Missing new user preferences for user: ${username}`);
   }
 
   try {
     const promises: Promise<UpdateItemCommandOutput | undefined>[] = [];
-    const remainingPreferencesToUpdate = Object.entries(
-      preferencesToUpdate
+    const nonAssetPreferences = Object.entries(
+      preferences
     ).reduce<{ key: string; value: any }[]>((acc, [key, value]) => {
       if (!value) return acc;
 
-      switch (key) {
-        case 'avatar': {
-          const {
-            name: avatarName,
-            previewUrl,
-            uploadDateTime
-          } = value as AvatarPreference;
+      const { name, previewUrl, uploadDateTime } = value;
+      const isAsset = !!previewUrl && !!uploadDateTime;
 
-          if (
-            avatarName === CUSTOM_AVATAR_NAME &&
-            previewUrl &&
-            uploadDateTime
-          ) {
-            promises.push(
-              new Promise<UpdateItemCommandOutput>(async (resolve, reject) => {
-                try {
-                  await dynamoDbClient.send(
-                    new UpdateItemCommand({
-                      UpdateExpression: `SET channelAssets.avatar = if_not_exists(channelAssets.avatar, :emptyMap)`,
-                      Key: { id: convertToAttr(sub) },
-                      ExpressionAttributeValues: {
-                        ':emptyMap': convertToAttr({})
-                      },
-                      TableName: process.env.CHANNELS_TABLE_NAME!
-                    })
-                  );
+      if ((previewUrl && !uploadDateTime) || (!previewUrl && uploadDateTime)) {
+        const missingKey = !previewUrl ? 'previewUrl' : 'uploadDateTime';
 
-                  const updateExpression = `SET ${[
-                    'avatar = :avatarName',
-                    'channelAssets.avatar.#url = :previewUrl',
-                    'channelAssets.avatar.#lastModified = :lastModified'
-                  ].join(', ')}`;
+        throw new Error(
+          `${missingKey} is missing from the asset preference data`
+        );
+      }
 
-                  const result = dynamoDbClient.send(
-                    new UpdateItemCommand({
-                      UpdateExpression: updateExpression,
-                      ConditionExpression:
-                        'attribute_not_exists(channelAssets.avatar.#lastModified) or (channelAssets.avatar.#lastModified < :lastModified)',
-                      Key: { id: convertToAttr(sub) },
-                      ExpressionAttributeValues: {
-                        ':avatarName': convertToAttr(avatarName),
-                        ':previewUrl': convertToAttr(previewUrl),
-                        ':lastModified': convertToAttr(
-                          new Date(uploadDateTime).getTime()
-                        )
-                      },
-                      ExpressionAttributeNames: {
-                        '#url': 'url',
-                        '#lastModified': 'lastModified'
-                      },
-                      TableName: process.env.CHANNELS_TABLE_NAME!
-                    })
-                  );
+      if (isAsset) {
+        promises.push(
+          new Promise<UpdateItemCommandOutput>(async (resolve, reject) => {
+            try {
+              await dynamoDbClient.send(
+                new UpdateItemCommand({
+                  UpdateExpression: `SET channelAssets.#${key} = if_not_exists(channelAssets.#${key}, :emptyMap)`,
+                  Key: { id: convertToAttr(sub) },
+                  ExpressionAttributeValues: { ':emptyMap': convertToAttr({}) },
+                  ExpressionAttributeNames: { [`#${key}`]: key },
+                  TableName: process.env.CHANNELS_TABLE_NAME!
+                })
+              );
 
-                  resolve(result);
-                } catch (error) {
-                  reject(error);
-                }
-              })
-            );
-          } else {
-            return [...acc, { key, value: avatarName }];
-          }
-          break;
-        }
-        default: {
-          const { name: preferenceName } = value as Preference;
+              const updateExpressionArr = [
+                `channelAssets.#${key}.#url = :previewUrl`,
+                `channelAssets.#${key}.#lastModified = :lastModified`
+              ];
 
-          return [...acc, { key, value: preferenceName }];
-        }
+              // For assets that contain a name (e.g. avatar), the corresponding attribute will also be updated
+              if (value.name) {
+                updateExpressionArr.push(`#${key} = :name`);
+              }
+
+              const updateExpression = `SET ${updateExpressionArr.join(', ')}`;
+              const result = dynamoDbClient.send(
+                new UpdateItemCommand({
+                  UpdateExpression: updateExpression,
+                  ConditionExpression: `attribute_not_exists(channelAssets.#${key}.#lastModified) or (channelAssets.#${key}.#lastModified < :lastModified)`,
+                  Key: { id: convertToAttr(sub) },
+                  ExpressionAttributeValues: {
+                    ...name && { ':name': convertToAttr(name) },
+                    ':previewUrl': convertToAttr(previewUrl),
+                    ':lastModified': convertToAttr(
+                      new Date(uploadDateTime).getTime()
+                    )
+                  },
+                  ExpressionAttributeNames: {
+                    '#url': 'url',
+                    '#lastModified': 'lastModified',
+                    [`#${key}`]: key
+                  },
+                  TableName: process.env.CHANNELS_TABLE_NAME!
+                })
+              );
+
+              resolve(result);
+            } catch (error) {
+              reject(error);
+            }
+          })
+        );
+      } else {
+        const { name: preferenceName } = value as Preference;
+
+        return preferenceName ? [...acc, { key, value: preferenceName }] : acc;
       }
 
       return acc;
     }, []);
 
-    if (remainingPreferencesToUpdate) {
-      // Update Dynamo user entry
-      await updateDynamoItemAttributes({
-        attributes: remainingPreferencesToUpdate,
-        primaryKey: { key: 'id', value: sub },
-        tableName: process.env.CHANNELS_TABLE_NAME!
-      });
-    }
+    // Update non-asset preferences
+    await updateDynamoItemAttributes({
+      attributes: nonAssetPreferences,
+      primaryKey: { key: 'id', value: sub },
+      tableName: process.env.CHANNELS_TABLE_NAME!
+    });
 
     await Promise.all(promises);
   } catch (error) {
@@ -148,7 +128,7 @@ const handler = async (
     return reply.send({ __type: CHANGE_USER_PREFERENCES_EXCEPTION });
   }
 
-  return reply.send(preferencesToUpdate);
+  return reply.send(preferences);
 };
 
 export default handler;
