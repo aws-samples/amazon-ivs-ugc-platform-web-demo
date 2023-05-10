14dafc568c7da5f4d6cd5c5565a2c96ecc3e9859
Michelle Sauder <45767844+michellesauder@users.noreply.github.com>
2023-05-10 10:55:20 -0700

    fix(aivs-1118): removed hover state when using a touchscreen device (#367)
    * fix: aivs-1118 update z-index
    
    * fix: remove hover states for touch devices
    
    * fix: disable hover on touchscreen devices
    
    * fix: revert change
    
    * fix: update format

--- web-ui/src/components/Tabs/Tab.jsx
+++ web-ui/src/components/Tabs/Tab.jsx
@@ -2,10 +2,13 @@ import { forwardRef, useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 import { clsm } from '../../utils';
+import { useResponsiveDevice } from '../../contexts/ResponsiveDevice';
 import Button from '../Button';
 
 const Tab = forwardRef(
   ({ isSelected, label, onClick, onKeyDown, panelIndex }, ref) => {
+    const { isTouchscreenDevice } = useResponsiveDevice();
+
     const onClickHandler = useCallback(
       () => onClick(panelIndex),
       [onClick, panelIndex]
@@ -18,16 +21,19 @@ const Tab = forwardRef(
     return (
       <Button
         {...(!isSelected ? { tabIndex: -1 } : {})}
+        disableHover={isTouchscreenDevice}
         ariaControls={`tabpanel-${panelIndex}`}
         ariaSelected={isSelected}
         className={clsm([
           'bg-lightMode-gray-tabButton',
           'dark:bg-darkMode-gray-tabButton',
           'dark:focus:bg-darkMode-gray-tabButton',
-          'dark:hover:bg-darkMode-gray-medium-hover',
           'focus:bg-lightMode-gray-tabButton',
-          'hover:bg-lightMode-gray-hover',
           'w-full',
+          !isTouchscreenDevice && [
+            'hover:bg-lightMode-gray-hover',
+            'dark:hover:bg-darkMode-gray-medium-hover'
+          ],
           isSelected && [
             'bg-lightMode-gray',
             'dark:bg-darkMode-gray-medium',
