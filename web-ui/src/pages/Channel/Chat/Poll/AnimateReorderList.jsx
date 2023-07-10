3ecd7a20b68e8693da9bf386b4e28ea4cc9ad4d2
Brandon Fajardo <brandonjfajardo@hotmail.com>
2023-07-10 15:49:39 -0700

    fix(animation): update

--- web-ui/src/pages/Channel/Chat/Poll/AnimateReorderList.jsx
+++ web-ui/src/pages/Channel/Chat/Poll/AnimateReorderList.jsx
@@ -8,8 +8,8 @@ export const calculateBoundingBoxes = (children) => {
   const boundingBoxes = {};
 
   React.Children.forEach(children, (child) => {
-    const domNode = child.ref.current;
-    const nodeBoundingBox = domNode.getBoundingClientRect();
+    const domNode = child?.ref?.current;
+    const nodeBoundingBox = domNode?.getBoundingClientRect();
 
     boundingBoxes[child.key] = nodeBoundingBox;
   });
