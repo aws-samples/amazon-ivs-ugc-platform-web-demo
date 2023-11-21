0d95435da677ee61d2d9533ba12303fc8f445320
Brandon Fajardo <brandonjfajardo@hotmail.com>
2023-11-20 16:19:27 -0800

    fix(modal): click away, open/close join modal

--- web-ui/src/components/Modal/Modal.jsx
+++ web-ui/src/components/Modal/Modal.jsx
@@ -11,17 +11,24 @@ import useFocusTrap from '../../hooks/useFocusTrap';
 import usePrevious from '../../hooks/usePrevious';
 import withPortal from '../withPortal';
 
-const Modal = ({ children, className }) => {
+const Modal = ({ children, className, onClickAway }) => {
   const { closeModal, type } = useModal();
   const { pathname } = useLocation();
   const modalRef = useRef();
   const prevPathname = usePrevious(pathname);
 
   useFocusTrap([modalRef]);
-  useClickAway([modalRef], () => {
-    if (type === MODAL_TYPE.STAGE_JOIN) return;
-    closeModal({ shouldRefocus: false });
-  });
+  useClickAway(
+    [modalRef],
+    () => {
+      if (onClickAway) {
+        onClickAway();
+      } else {
+        closeModal({ shouldRefocus: false });
+      }
+    },
+    type !== MODAL_TYPE.STAGE_JOIN
+  );
 
   // Close the modal on page change
   useEffect(() => {
@@ -53,11 +60,12 @@ const Modal = ({ children, className }) => {
   );
 };
 
-Modal.defaultProps = { className: '' };
+Modal.defaultProps = { className: '', onClickAway: null };
 
 Modal.propTypes = {
   children: PropTypes.node.isRequired,
-  className: PropTypes.string
+  className: PropTypes.string,
+  onClickAway: PropTypes.func
 };
 
 export default withPortal(Modal, 'modal', {
