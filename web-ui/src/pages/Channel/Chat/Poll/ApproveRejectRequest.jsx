c6aa3339afb8fbf10c527ab863c4772f1bd7ea83
srinith-ngenux <srinith.gujula@ngenux.com>
2024-01-16 17:23:10 +0530

    approve/reject styling fix

--- web-ui/src/pages/Channel/Chat/Poll/ApproveRejectRequest.jsx
+++ web-ui/src/pages/Channel/Chat/Poll/ApproveRejectRequest.jsx
@@ -7,8 +7,8 @@ const ApproveRejectRequest = ({joinRequestStatus}) => {
   const { requestJoin, requestAprrove, requestReject } = useChat();
 
   return (
-    <div className="flex flex-row w-full  left-1 top-3/4 absolute">
-      <div>
+    <div className="flex flex-row w-full left-1 top-3/4 absolute">
+      <div className='mr-3 mt-2'>
         {`${joinRequestStatus?.requestedUsername} raised hand to speak`}
       </div>
       <Button
@@ -17,9 +17,9 @@ const ApproveRejectRequest = ({joinRequestStatus}) => {
             bg-darkMode-green
             hover:dark:bg-darkMode-green-hover
             hover:bg-darkMode-green-hover
-            focus:bg-darkMode-green text-white mr-1 min-w-[46px] h-[40px]"
+            focus:bg-darkMode-green text-white mr-1 max-w-[44px] h-[36px] px-1"
       >
-        <Check style = {{ height: '34px', width: '34px'}}/>
+        <Check style = {{ height: '30px', width: '30px'}}/>
       </Button>
       <Button
         onClick={requestReject}
@@ -27,9 +27,9 @@ const ApproveRejectRequest = ({joinRequestStatus}) => {
             bg-darkMode-red
             hover:dark:bg-darkMode-red-hover
             hover:bg-darkMode-red-hover
-            focus:bg-darkMode-red text-white min-w-[46px] h-[40px]"
+            focus:bg-darkMode-red text-white max-w-[44px] h-[36px] px-1"
       >
-        <Close style = {{ height: '34px', width: '34px'}}/>
+        <Close style = {{ height: '30px', width: '30px'}}/>
       </Button>
     </div>
   );
